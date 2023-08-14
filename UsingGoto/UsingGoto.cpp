ac728713db7b1250b7e85d8ee596d8676dcc60cf
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-14 21:47:51 +0500

    Добавьте файлы проекта.

---
+++ UsingGoto/UsingGoto.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки консоли:
+    system("chcp 1251 > nul");
+    // Целочисленные переменные:
+    int n = 10, s = 0, k = 1;
+start: // Метка
+    s += k * k;
+    if (k < n) {
+        k++;
+        // Переход к месту обозначенному меткой:
+        goto start;
+    }
+    // Отображение результата вычисления:
+    cout << "Сумма квадратов чисел от одного до " << n << ": " << s << endl;
+    // Задержка консоли окна:
+    system("pause > nul");
+    return 0;
+}
